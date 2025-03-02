2b5f999bdf60171f903f3e2f85c217614210198c
sasha <sjlarson92@gmail.com>
2025-03-02 16:55:19 -0500

    add return types for Item

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,15 +1,43 @@
 import items from "../../Items.js";
 
+// Item schema
+const Item = {
+    type: 'object',
+    properties: {
+        id: {type: 'string'},
+        name: {type: 'string'}
+    }
+}
+
+const ItemType = {
+    schema: {
+        response: {
+            200: {
+                Item
+            }
+        }
+    }
+}
+const ItemArrayType = {
+    schema: {
+        response: {
+            200: {
+                type: 'array',
+                items: Item
+            }
+        }
+    }
+}
 async function routes (fastify, options) {
     fastify.get('/', async () => {
         return { hello: 'world' }
     })
 
-    fastify.get('/items', (request, reply) => {
+    fastify.get('/items', ItemArrayType, (request, reply) => {
         reply.send(items)
     })
 
-    fastify.get('/items/:id', (request, reply) => {
+    fastify.get('/items/:id', ItemType, (request, reply) => {
         const {id} = request.params
         const item = items.find(item => item.id === id)
         reply.send(item)
